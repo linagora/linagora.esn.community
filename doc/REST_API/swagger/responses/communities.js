37d343723fcf02f6529462088687b02c92112319
Tuan Tuan LE <tlcong@linagora.com>
2020-02-12 10:42:30 +0700

    #11 move back ct_raw_data and ct_avatar to core swagger since they are required by documentation for collaboration

--- doc/REST_API/swagger/responses/communities.js
+++ doc/REST_API/swagger/responses/communities.js
@@ -64,19 +64,6 @@
   *         }
   *   ct_error_conflict:
   *     description: Conflict. A community already exists with this title in the domain.
-  *   ct_avatar:
-  *     description: OK. With the recorded image ID
-  *     schema:
-  *       type: object
-  *       properties:
-  *         "_id":
-  *           type: string
-  *           format: uuid
-  *     examples:
-  *       application/json:
-  *         {
-  *           "_id": '9f888058-e9e6-4915-814b-935d5b88389d'
-  *         }
   *   ct_member:
   *     description: OK - Current user is a community member and user is a member.
   *     schema:
